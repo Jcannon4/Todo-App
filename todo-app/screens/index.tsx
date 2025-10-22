7777bfebc2c1fe565f1b3bd4c55a1cc953955ea1
Jackson Contreras <jacksoncontreras4@gmail.com>
2025-10-22 16:11:55 -0700

    removed quickvertical animation file and all unused imports. did some code refactoring

--- todo-app/screens/index.tsx
+++ todo-app/screens/index.tsx
@@ -1,9 +1,5 @@
-import AddUserButton from "../components/AddButton";
 import { Link } from "expo-router";
-import InputField from "../components/InputField";
-import consoleLog, { logWarn, logObject } from "../app/utils";
-import { View, Button } from "react-native";
-import QuickVerticalAnimation from "@/components/QuickVerticalAnimation";
+import { View } from "react-native";
 
 export default function Index() {
   return (
@@ -14,20 +10,6 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      {/* <Button
-        onPress={() => {
-          consoleLog("Hello");
-          logWarn("Example warning");
-          logObject(true);
-        }}
-        title="Learn More"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-      <InputField placeholder="username" />
-      <InputField placeholder="password" />
-      <AddUserButton /> */}
-      {/* <QuickVerticalAnimation/> */}
       <Link href="/todoList">Todo List</Link>
     </View>
   );
